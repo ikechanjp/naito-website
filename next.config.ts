762ecdc7bf48ee1db268d3fdce1c01808011672b
ikechanjp <noriyoshi.ikeda@gmail.com>
2025-07-19 22:31:52 +0900

    V2.2.7

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
-  basePath: '/naito-website',
-  assetPrefix: '/naito-website',
   images: {
     unoptimized: true,
   },
